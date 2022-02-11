4338b29a8ffe85a4a334892fd8849f33abfa980c
Alexis Leblanc <e1782223@cmaisonneuve.qc.ca>
2022-02-11 16:49:31 -0500

    test

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="site__footer">
     <p class="footer__presentation">Un site web fait par une élève du TIM à l'aide de Wordpress.</p>
-    <h3 class="footer__author">Fait par Alexis Leblanc</h3>
+    <h3 class="footer__author">Fait par Alexis Leblanc </h3>
     <?php wp_nav_menu(array("menu" => "simple", "container" => "nav", "container_class" => "footer")); ?>
 </footer>
 </body>
