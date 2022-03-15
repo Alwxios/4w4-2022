464aaf537f0c91840aa33601771a4c003b08e08a
Alexis Leblanc <alexis-leblanc@hotmail.fr>
2022-03-15 15:39:04 -0400

    Nouvelle version

--- category-cours.php
+++ category-cours.php
@@ -1,31 +1,32 @@
 <?php get_header() ?>
-<main class="site__main">
-    <h1>category-cours.php</h1>
-    <?php if(have_posts()): ?>
-        <section class="cours">
-            <?php while(have_posts()): the_post(); ?>
-            <article class="cours__carte">
+<main class="principal">
+    <section class="formation">
+        <h2 class="formation__titre">Liste des cours du programme TIM</h2>
+        <div class="formation__liste">
+            <?php if (have_posts()):
+                while (have_posts()): the_post(); ?>
+            <article class="formation__cours">
                 <?php
-                    $titre = get_the_title();
-                    $titreCourt = substr(get_the_title(), 8);
-                    $titreCourt = substr($titreCourt, 0, strrpos($titreCourt, '('));
-
-                    $posDebutHeures = strrpos($titre, '(') + 1;
-                    $posFinHeures = strrpos($titre, ')');
-                    $nbHeures = substr($titre, $posDebutHeures, $posFinHeures - $posDebutHeures);
+                $titre = get_the_title();
+                $titreFiltreCours = substr($titre, 7, -6);
+                $nbHeures = substr($titre, -6);
+                $sigleCours = substr($titre, 0, 7);
+                $descCours = get_the_excerpt();
                 ?>
+
                 <?php the_post_thumbnail("thumbnail"); ?>
-                <h2 class="carte__titre"><?= $titreCourt; ?></h2>
-                <a href="<?php echo get_permalink() ?>">
-                    <?php $titreCourt ?>
-                </a>
-                
-                <p class="carte__code"><?= substr(get_the_title(), 0, 7) ?></p>
-                <p><?= $nbHeures; ?></p>
-                <p class="carte__description"><?= get_the_excerpt() ?></p>
+                <h3 class="cours__titre">
+                    <a href="<?php echo get_permalink(); ?>">
+                        <?= $titreFiltreCours; ?>
+                    </a>
+                </h3>
+                <div class="cours__nbre-heure"><?= $nbHeures; ?></div>
+                <p class="cours__sigle"><?= $sigleCours; ?> </p>
+                <p class="cours__desc"> <?= $descCours; ?></p>
             </article>
-            <?php endwhile; ?>
-        </section>
-    <?php endif; ?>
+            <?php endwhile ?>
+            <?php endif ?>
+        </div>
+    </section>
 </main>
 <?php get_footer() ?>
---
+++ readme.md
@@ -0,0 +1,13 @@
+# 4w4 - Conception d'interface et développement Web
+### Auteur : Alexis Leblanc
+### TP1
+
+Développement des modules:
+ - search.php
+ - searchform.php
+Développement d'un nouveau mixin link_menu
+
+ Début d'une animation
+
+Pour modifier readme.md
+https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
