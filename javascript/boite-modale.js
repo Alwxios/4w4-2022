d79fc0aba0a9cb300fc8c0812d11b2a2bbca97ce
Alexis Leblanc <alexis-leblanc@hotmail.fr>
2022-04-22 15:55:14 -0400

    texte boite modale - S12, C2 - AL

--- javascript/boite-modale.js
+++ javascript/boite-modale.js
@@ -2,6 +2,7 @@
     //console.log("vive la boite modale");
     let boite__modale = document.querySelector(".boite__modale");
     let cours__desc__ouvrir = document.querySelectorAll('.cours__desc__ouvrir');
+    let cours__desc__texte = document.querySelector('.boite__modale__texte');
     //console.log(cours__desc__ouvrir.length);
 
     for (const bout of cours__desc__ouvrir) {
@@ -10,6 +11,8 @@
             //console.log(this.parentNode.parentNode.className)
             boite__modale.classList.add('ouvrir')
             //console.log(boite__modale.classList)
+            //console.log(this.parentNode.parentNode.children[0].innerHTML)
+            cours__desc__texte.innerHTML = this.parentNode.parentNode.children[0].innerHTML
         })
     }
     const btnFermerModal = document.querySelector('.boite__modale__fermeture');
